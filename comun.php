8978a7898c36b23bbad6bb900f8d72b95985a099
Denmys <movisoftvc@gmail.com>
2023-12-16 18:01:40 -0500

    Agregando Dependencias Comunes

---
+++ comun.php
@@ -0,0 +1,23 @@
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="index, follow">
+    <meta property="og:title" content="Servicios informáticos personalizados | MovisoftVC | Impulsa la eficiencia">
+    <meta name="description" content="MoviSoftVC ofrece soluciones informáticas estratégicas, desarrollo de software a medida, 
+    consultoría tecnológica y servicios de soporte técnico para empresas. Impulsa la eficiencia 
+    y rentabilidad con nuestras soluciones personalizadas.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.movisoftvc.com/">
+    <meta property="og:site_name" content="MoviSoft Servicios Informáticos">
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:image:width" content="114">
+    <meta property="og:image:height" content="114">
+    <meta property="og:image:type" content="image/webp" />
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <link rel="canonical" href="https://www.movisoftvc.com/">
+
+
+    <link href="./src/css/movisoft.css" rel="stylesheet"/>
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"/>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es" prefix="og: https://ogp.me/ns#">
+<head>
+
+    
+
+    <title>MoviSoft</title> 
+</head>
+<body>
+    
+</body>
+</html>
