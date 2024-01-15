a21ca1eed8a28cfa6fd1d5052346036cc3931e70
Denmys <movisoftvc@gmail.com>
2024-01-15 09:10:05 -0500

    Modificando Sección Inicio

--- app/vistas/SeccionInicio.php
+++ app/vistas/SeccionInicio.php
@@ -3,7 +3,7 @@
         <h1>Bienvenido a MoviSoft</h1>
 
         <div class="change-text">
-            <h3>Tu Socio Tecnológico Confiable</h3>
+            <h3>Infraestructura tecnológica y servicios informáticos</h3>
             <h3>
                 <span class="word">Servicios de Mantenimiento</span>
                 <span class="word">Mantenimiento de Sistemas Informáticos</span>
@@ -19,5 +19,34 @@
                 <span class="word">Venta de Equipos Eléctricos, Electrónicos y de Consumo</span>
             </h3>
         </div>
+        <p>Descubre qué es el mantenimiento de hardware y su importancia para cuidar y reemplazar oportunamente 
+            los componentes físicos de una computadora.</p>
+        
+        <div class="info-box">
+            <div class="email-info">
+                <h5>Email:</h5>
+                <span>administrador@movisoftvc.com</span>
+            </div>
+            <div class="email-info">
+                <h5>Teléfono:</h5>
+                <span>+53 52378034</span>
+            </div>
+        </div>
+        <div class="btn-box">
+            <a href="" class="btn">Catálogos</a>
+            <a href="" class="btn">Contáctenos</a>
+        </div>
+        <div class="social-icons">
+            <a href=""><i class='bx bxl-facebook'></i></a>
+            <a href=""><i class='bx bxl-instagram'></i></a>
+            <a href=""><i class='bx bxl-twitter' ></i></a>
+        </div>
     </div>
+
+    <div class="home-image">
+        <div class="img-box">
+            <img src="./public/src/img/tech.png" alt="home-image">
+        </div>
+    </div>
+
 </section>
