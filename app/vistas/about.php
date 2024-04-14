fe79c32a6c4c93d6a926bec6ca7ed7b5ff16a8c1
Denmys <movisoftvc@gmail.com>
2024-04-14 09:24:30 -0400

    Terminando Seccion About

--- app/vistas/about.php
+++ app/vistas/about.php
@@ -15,21 +15,24 @@
         </div>
     </div>
     <div class="about-content">
-        <span>Potenciando tu negocio con tecnología</span>
+        <span><strong>Potenciando tu negocio con tecnología</strong></span>
+        <br>
+        <br>
         <h2>Sobre MoviSoft</h2>
-        <h3>Su Mejor Aliado</h3>
-        <p>Somos una empresa de servicios informáticos comprometida con ofrecer soluciones innovadoras y personalizadas a nuestros clientes. 
+        <br>
+        <h3>Por qué su Mejor Aliado ?</h3>
+        <p>Porque somos una empresa de servicios informáticos comprometida con ofrecer soluciones innovadoras y personalizadas a nuestros clientes. 
             Desde nuestra fundación, nos hemos dedicado a ayudar a empresas y particulares a alcanzar sus metas a través de la tecnología.</p>
 
-        <h3>Nuestro Equipo</h3>
+        <h3>Equipo</h3>
         <p>Contamos con un equipo de profesionales apasionados por la tecnología, cada uno experto en su campo. 
             Nuestra diversidad de habilidades nos permite abordar proyectos de cualquier tamaño y complejidad con confianza y eficacia.</p>
 
-        <h3>Nuestra Misión</h3>
+        <h3>Misión</h3>
         <p>Nuestra misión es proporcionar a nuestros clientes soluciones informáticas que impulsen su éxito empresarial. 
             Nos esforzamos por mantenernos a la vanguardia de la tecnología y adaptarnos constantemente a las necesidades cambiantes del mercado.</p>
 
-        <h3>Nuestra Visión</h3>
+        <h3>Visión</h3>
         <p>Buscamos ser líderes en el sector de servicios informáticos, reconocidos por nuestra excelencia, innovación y compromiso con la satisfacción del cliente. 
             Queremos ser el socio tecnológico preferido para empresas de todos los tamaños y sectores.</p>
 
