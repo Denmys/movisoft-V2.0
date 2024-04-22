ef059e356355e8bc0e72069b23e128745dd2b4dd
Denmys <movisoftvc@gmail.com>
2024-04-22 15:57:59 -0400

    Terminando Seccion Contacto

---
+++ app/vistas/contacto.php
@@ -0,0 +1,18 @@
+<section id="contacto" class="contacto">
+    <div class="main-text">
+        <span>Déjanos tus datos y un experto se pondrá en contacto contigo para resolver todas tus dudas y ofrecerte las mejores soluciones a medida.</span>
+        <h2>Contáctanos</h2>
+    </div>
+
+    <form action="#">
+        <input type="text" placeholder="Nombre y Apellidos">
+        <input type="text" placeholder="Correo Electrónico">
+        <input type="text" placeholder="Centro de Trabajo">
+        <input type="number" placeholder="Número de Teléfono">
+        <textarea name="" id="" cols="30" rows="10" placeholder="Su Mensaje"></textarea>
+        <div class="btn-box formBtn">
+            <button type="submit" class="btn">Enviar</button>
+        </div>
+    </form>
+
+</section>
